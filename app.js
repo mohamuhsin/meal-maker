7deec344f9d5521f131eb5cc55aec09ecf97090b
obre-bjorn <obre.bjorn@gmail.com>
2023-10-16 23:01:41 +0300

    Bjorn's Solution

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-// Moha's Solution
+// Bjorn's Solution
 
-//Wamata's Solution
-const menu = {
- _meal: '',
- _price: 0,
+const menu = {};
+menu._meal = '';
 
- set meal_setter(mealToCheck) {
-    if (typeof mealToCheck === 'string') {
-        return menu._meal = mealToCheck
-    }
-},
+menu._price = 0;
 
- set price_setter(priceToCheck) {
-    if (typeof priceToCheck === 'number') {
-        return menu._price = priceToCheck
-    }
- },
- get todaysSpecial() {
-    if (menu._meal !== '' && menu._meal !== null && menu._price !== null) {
-       console.log(`Today’s Special is ${menu._meal} for ${menu._price} `)
-    }
-    else {
-        console.log(`Meal or price was not set correctly!`)
-    }
-    
- }
+menu._meal = 42;      // Setting _meal to a number
+menu._price = '20';   // Setting _price to a string
+console.log(menu);    // Logging the menu object to demonstrate the issue
+
+
+menu.setMeal = function(mealToCheck) {
+  if (typeof mealToCheck === 'string') {
+    this._meal = mealToCheck;
+  }
 }
+
+menu.setPrice = function(priceToCheck) {
+  if (typeof priceToCheck === 'number') {
+    this._price = priceToCheck;
+  }
+}
+
+menu.todaysSpecial = function() {
+  if (this._meal && typeof this._price === 'number') {
+    return `Today's Special is ${this._meal} for $${this._price}!`;
+  } else {
+    return 'Meal or price was not set correctly!';
+  }
+}
+
+menu.setMeal('Spaghetti');
+menu.setPrice(5);
+console.log(menu.todaysSpecial()); // Logs "Today's Special is Spaghetti for $5!"
