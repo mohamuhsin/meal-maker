1224017a136cec5bc6446c85c53844ec886d64b9
mohamuhsin <muhsinohoho@outlook.com>
2023-10-17 20:08:59 +0300

    our work

--- app.js
+++ app.js
@@ -1,28 +1,49 @@
 // Moha's Solution
+const menuI = {
+  _meal: "",
+  _price: 0,
+
+  set meal_setter(mealToCheck) {
+    if (typeof mealToCheck === "string") {
+      return (menu._meal = mealToCheck);
+    }
+  },
+
+  set price_setter(priceToCheck) {
+    if (typeof priceToCheck === "number") {
+      return (menu._price = priceToCheck);
+    }
+  },
+  get todaysSpecial() {
+    if (menu._meal !== "" && menu._meal !== null && menu._price !== null) {
+      console.log(`Today’s Special is ${menu._meal} for ${menu._price} `);
+    } else {
+      console.log(`Meal or price was not set correctly!`);
+    }
+  },
+};
 
 //Wamata's Solution
 const menu = {
- _meal: '',
- _price: 0,
+  _meal: "",
+  _price: 0,
 
- set meal_setter(mealToCheck) {
-    if (typeof mealToCheck === 'string') {
-        return menu._meal = mealToCheck
+  set meal_setter(mealToCheck) {
+    if (typeof mealToCheck === "string") {
+      return (menu._meal = mealToCheck);
     }
-},
+  },
 
- set price_setter(priceToCheck) {
-    if (typeof priceToCheck === 'number') {
-        return menu._price = priceToCheck
+  set price_setter(priceToCheck) {
+    if (typeof priceToCheck === "number") {
+      return (menu._price = priceToCheck);
     }
- },
- get todaysSpecial() {
-    if (menu._meal !== '' && menu._meal !== null && menu._price !== null) {
-       console.log(`Today’s Special is ${menu._meal} for ${menu._price} `)
+  },
+  get todaysSpecial() {
+    if (menu._meal !== "" && menu._meal !== null && menu._price !== null) {
+      console.log(`Today’s Special is ${menu._meal} for ${menu._price} `);
+    } else {
+      console.log(`Meal or price was not set correctly!`);
     }
-    else {
-        console.log(`Meal or price was not set correctly!`)
-    }
-    
- }
-}
+  },
+};
